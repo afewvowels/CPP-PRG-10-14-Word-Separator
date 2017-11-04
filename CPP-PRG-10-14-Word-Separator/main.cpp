ad84bc22d7ae15e01fe12be462250529d26fe8c2
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-04 15:48:50 -0400

    Prompt written.

--- CPP-PRG-10-14-Word-Separator/main.cpp
+++ CPP-PRG-10-14-Word-Separator/main.cpp
@@ -5,11 +5,19 @@
 //  Created by Keith Smith on 11/4/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  Write a program that accepts as input a sentence in which all of the words are run
+//  together, but the first character of each word is uppercase. Convert the sentence to a
+//  string in which the words are separated by spaces and only the first word starts with
+//  an uppercase letter. For example, the string "StopAndSmellTheRoses." would be converted
+//  to "Stop and smell the roses."
+//
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+
     return 0;
 }
